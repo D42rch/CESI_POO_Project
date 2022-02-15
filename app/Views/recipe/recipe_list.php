eec97539407a7ceeb3f8dc709a8bca17ed14780c
D42rch <gguichard25@gmail.com>
2022-02-15 12:08:54 +0100

    add style to recipe list

--- app/Views/recipe/recipe_list.php
+++ app/Views/recipe/recipe_list.php
@@ -14,7 +14,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title><?php echo $name; ?></title>
     <link rel="shortcut icon" type="image/png" href="<?php echo base_url('favicon.ico'); ?>" />
-    <link rel="shortcut icon" type="text/css" href="<?php echo base_url('/css/style.css'); ?>" />
+    <link rel="stylesheet" type="text/css" href="<?php echo base_url('/css/style.css'); ?>" />
     
 </head>
 
